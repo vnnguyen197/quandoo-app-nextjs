121917ec3b8333ae3b35c3248d0f66686f6a2d63
Van Nguyen <nguyen.nv@techchain.vn>
2022-06-01 22:44:29 +0700

    update code slide find restaurants

--- pages/SlideFind/component/index.tsx
+++ pages/SlideFind/component/index.tsx
@@ -9,21 +9,19 @@ export default function SildeFind(props: SildeFindProps) {
   return (
     <div className={style.slide}>
       <div className={style.slideMain}>
-
         <div className={style.slideImg}>
-          <Image className={style.images} src={logoSlide} alt="logoSlide"/>
-        </div>
-
-        <div className={style.slideContent}>
-          <h1> It is time to treat yourseft</h1>
-          <h2> Let is book you a table - the Quandoo way. </h2>
-          <div className={style.contentBottom}>
-            <input placeholder="Find restaurant or cuisines" type="text" />
-            <input placeholder="Singapore" type="text" />
-            <button type="submit">Find</button>
+          <div className={style.slideContent}>
+            <div className={style.content}>
+              <h1> It's time to treat yourseft</h1>
+              <h2> Let's book you a table - the Quandoo way. </h2>
+            </div>
+            <div className={style.contentBottom}>
+              <input className={style.inputFind} placeholder="Find restaurant or cuisines" type="text"/>
+              <input className={style.inputSingapore} placeholder="Singapore" type="text" />
+              <button type="submit">Find</button>
+            </div>
           </div>
         </div>
-
       </div>
     </div>
   );
