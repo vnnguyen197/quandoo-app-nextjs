57c7850093090e79a7a7a190cdfb297df951129e
Van Nguyen <nguyen.nv@techchain.vn>
2022-06-02 13:29:08 +0700

    update code reponsive find restaurants

--- pages/SlideFind/component/index.tsx
+++ pages/SlideFind/component/index.tsx
@@ -1,28 +1,31 @@
 import Image from "next/image";
 import * as React from "react";
-import logoSlide from "../../../public/images/home_hero_bg.avif";
 import style from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export interface SildeFindProps {}
 
 export default function SildeFind(props: SildeFindProps) {
   return (
-    <div className={style.slide}>
       <div className={style.slideMain}>
         <div className={style.slideImg}>
           <div className={style.slideContent}>
             <div className={style.content}>
-              <h1> It's time to treat yourseft</h1>
+              <h1> It's time to treat yourseft.</h1>
               <h2> Let's book you a table - the Quandoo way. </h2>
-            </div>
-            <div className={style.contentBottom}>
               <input className={style.inputFind} placeholder="Find restaurant or cuisines" type="text"/>
               <input className={style.inputSingapore} placeholder="Singapore" type="text" />
+              <div className={style.search}>
+                <span className={style.icon}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" fixedWidth />
+                </span>
+                <input className={style.inputSearch} placeholder="Search" type="search" />
+              </div>
               <button type="submit">Find</button>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
