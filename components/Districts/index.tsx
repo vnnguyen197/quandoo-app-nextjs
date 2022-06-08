dd82dc8cf6d2822645e7aca9a630056f3e9952c7
Van Nguyen <nguyen.nv@techchain.vn>
2022-06-08 13:35:42 +0700

    update fix responsive

--- components/Districts/index.tsx
+++ components/Districts/index.tsx
@@ -10,26 +10,14 @@ export default function Districts(props: IDistrictsProps) {
         <h2 className={style["content__title--h2"]}>{districts.h2}</h2>
         <p className={style["content__title--p"]}>{districts.h3}</p>
       </div>
-        <div className={style.content__row}>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn1"]}`}>{districts.button1}</button>
-          </div>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn2"]}`}>{districts.button2}</button>
-          </div>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn3"]}`}>{districts.button3}</button>
-          </div>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn4"]}`}>{districts.button4}</button>
-          </div>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn5"]}`}>{districts.button5}</button>
-          </div>
-          <div className={style.content__div}>
-            <button className={`${style.content__buttons} ${style["content__buttons--btn6"]}`}>{districts.button6}</button>
-          </div>
-        </div>
+      <div className={style.content__row}>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn1"]}`}>{districts.button1}</button>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn2"]}`}>{districts.button2}</button>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn3"]}`}>{districts.button3}</button>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn4"]}`}>{districts.button4}</button>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn5"]}`}>{districts.button5}</button>
+        <button className={`${style.content__buttons} ${style["content__buttons--btn6"]}`}>{districts.button6}</button>
+      </div>
     </div>
   );
 }
